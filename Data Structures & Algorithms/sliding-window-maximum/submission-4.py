71a4ae6c6d0ea67e5e2f82678efe1517c5da0e98
zyy-cc <60497827+zyy-cc@users.noreply.github.com>
2026-09-19 23:23:33 -0500

    Add: sliding-window-maximum - submission-4

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-4.py
@@ -0,0 +1,28 @@
+# from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        
+        left = right = 0
+        res = []
+        q = deque()
+
+        while right < len(nums):
+            # if the new element is larger than the past element, we need to delete the small one. 
+            # because it means the smaller one can't be the maximum value in the window
+            while q and nums[q[-1]] < nums[right]:
+                q.pop()
+            
+            q.append(right)
+            # store idx is to check if the index is still in this window or not
+            if q[0] < left:
+                q.popleft()
+            
+            # only when the length is valid, we record the value
+            if right - left + 1 == k:
+                res.append(nums[q[0]])
+                left += 1
+            right += 1
+        return res
+
+
+        
